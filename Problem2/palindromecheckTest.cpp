4bacc14c331a46032700ece45de03596177cf2a0
Bayu Putra Ibana <bayuputraibana@mail.ugm.ac.id>
2024-11-25 14:04:23 +0700

    Update palindromecheckTest.cpp

--- Problem2/palindromecheckTest.cpp
+++ Problem2/palindromecheckTest.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 int main(){
     string str, reversed;
-    cout << "Enter a string/word: ";
-    getline(cin,str);
-
+    str = "racecar";
+    
     for(int i = str.length() - 1; i >= 0; i--){
         reversed += str[i];
     }
-
+    
+    bool testPassed = true;
+    
     if(reversed == str){
         cout << str << " is a palindrome." << endl;
+        testPassed = true;
     }else{
         cout << str << " is not a palindrome." << endl;
+        testPassed = false;
+    }
+
+    if(testPassed){
+        cout << "TEST SUCCESSFUL!" << endl;
+    }else{
+        cout << "TEST UNSUCCESSFUL!" << endl;
     }
 
     return 0;
